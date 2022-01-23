c6a414abd67d3480a3cdda0b46b14e851238e353
Astecom <astecom@mail.ru>
2022-01-24 00:15:28 +0400

    Initial commit

---
+++ src/main/java/io/github/asewhy/support/Identifier.java
@@ -0,0 +1,14 @@
+package io.github.asewhy.support;
+
+import java.lang.annotation.*;
+
+/**
+ * В случае если сущность запроса будет состоять как член какой-либо коллекции, то эта аннотация
+ * будет указывать на поле-идентификатор этой сущности работает только в {@link io.github.asewhy.conversions.ConversionMutator}
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Identifier {
+
+}
